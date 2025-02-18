7b4ecf8e416afa001ac2ab436f257664f1d37e34
alunosNET <alunosnet@gmail.com>
2025-02-18 12:59:13 +0000

    Sets (conjuntos)

---
+++ recomendacoes.py
@@ -0,0 +1,10 @@
+"""Pretendemos criar um sistema de recomendações para compras
+Para isso devemos recomendar produtos que o joão ainda não comprou mas que pertencem a uma categoria de produtos que já tenha"""
+#compras do joão
+joão={'tv','sapatos','tablet'}
+#produtos por categoria
+roupa      = {'calções','casaco','tshirt'}
+calcado    = {'sapatos','botas','sapatilhas'}
+eletronica = {'tv','tablet','pc','xbox'}
+
+#recomendar produtos cuja categoria pertence a um produto que o joão já comprou mas cujo produto ainda não comprou
